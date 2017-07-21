1897ea3203c812091eeb7047d0a71982762f3eb8
amarjit <datta.amarjit@gmail.com>
2017-07-21 15:08:24 -0400

    Refactored code for separating mysql codes from the main servlet code.

--- src/main/java/servlets/RegistrationServlet.java
+++ src/main/java/servlets/RegistrationServlet.java
@@ -1,8 +1,7 @@
 package servlets;
 
-import objects.Person;
-import utils.mysql.DBConnection;
-import utils.mysql.InsertNewUser;
+import contents.Person;
+import utils.mysql.MySQLInterface;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,30 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.*;
 
 public class RegistrationServlet extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    Connection connection = null;
-
     Person person = new Person(request);
 
-    try {
-      connection = DBConnection.getConnection();
-      System.out.println("Database connected!");
-      InsertNewUser insertNewUser = new InsertNewUser(person, connection);
-      insertNewUser.createNewUser();
-/*      statement = connection.createStatement();
-      resultSet = statement.executeQuery("select * from persons");
-      while (resultSet.next()) {
-        System.out.println("PersonId: " + resultSet.getString(1) + " and Email: " + resultSet.getString(2));
-      }*/
-      DBConnection.closeConnection();
-    } catch (SQLException e) {
-      throw new IllegalStateException("Cannot connect the database!", e);
-    } catch (ClassNotFoundException ex) {
-      throw new IllegalStateException("Could not find class!", ex);
-    }
+    MySQLInterface insertNewUser = new MySQLInterface(person);
+    insertNewUser.createNewUser();
 
     /*A new user has been created successfully*/
     response.setContentType("text/html");
---
+++ src/main/java/utils/mysql/MySQLInterface.java
@@ -0,0 +1,51 @@
+package utils.mysql;
+
+import contents.Person;
+
+import java.sql.*;
+
+public class MySQLInterface {
+  private Person person;
+
+  public MySQLInterface(Person person) {
+    this.person = person;
+  }
+
+  public boolean createNewUser() {
+    try {
+      Connection connection = DBConnection.getConnection();
+      System.out.println("Database connected!");
+
+      String query = "insert into persons (email, lastName, firstName, address1, address2, city, state, zipcode, phone)"
+          + " values (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+      PreparedStatement insertPersonStatement = connection.prepareStatement(query);
+      insertPersonStatement.setString(1, person.getEmail());
+      insertPersonStatement.setString(2, person.getLastName());
+      insertPersonStatement.setString(3, person.getFirstName());
+      insertPersonStatement.setString(4, person.getAddress1());
+      insertPersonStatement.setString(5, person.getAddress2());
+      insertPersonStatement.setString(6, person.getCity());
+      insertPersonStatement.setString(7, person.getState());
+      insertPersonStatement.setInt(8, person.getZip());
+      insertPersonStatement.setString(9, person.getPhone());
+      insertPersonStatement.execute();
+
+    /*Generate password and store that*/
+      String query2 = "insert into person_passwd (PersonID, Email, PassWd)"
+          + " values (LAST_INSERT_ID(), ?, ?)";
+
+      PreparedStatement insertPasswordStatement = connection.prepareStatement(query2);
+      insertPasswordStatement.setString(1, person.getEmail());
+      insertPasswordStatement.setString(2, person.getPassword());
+      insertPasswordStatement.execute();
+
+      DBConnection.closeConnection();
+    } catch (SQLException e) {
+      throw new IllegalStateException("Cannot connect the database!", e);
+    } catch (ClassNotFoundException ex) {
+      throw new IllegalStateException("Could not find class!", ex);
+    }
+    return true;
+  }
+}
